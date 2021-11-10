fbb17e9a129fd27de8b35366cc1c3753196ff81d
Yam Ebgui <yam.ebgui@gmail.com>
2021-11-10 21:16:05 +0200

    Add functions to show persons list

--- front/index.js
+++ front/index.js
@@ -1,10 +1,16 @@
 // const bootstrap = require("bootstrap");
 // const styles = require("./style.css");
 //import axios from "axios";
+
+//const { Button } = require("bootstrap");
+
+//const { default: axios } = require("axios");
+
 //Dom Element Section
 const nameInput = document.getElementById("inputName");
 const phoneInput = document.getElementById("inputPhone");
 const addButton = document.getElementById("addBtn");
+const personsListElement = document.getElementById("personsList");
 
 const serverUrl = "http://localhost:3001/";
 
@@ -23,6 +29,7 @@ const addRequest = async (name, phoneNumber) => {
       console.log(result);
     })
     .catch((err) => {
+      alert("You are trying to add exist person");
       console.log(err);
     });
 };
@@ -36,4 +43,58 @@ const addNewPerson = (event) => {
   }
 };
 
+const getAllPersonsRequest = async () => {
+  axios
+    .get(`${serverUrl}api/persons`)
+    .then((result) => {
+      cleanPersonsList();
+      displayAllPersons(result.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const displayAllPersons = (personsList) => {
+  for (let i = 0; i < personsList.length; i++) {
+    const deleteBtn = createElement("button", "🗑️", ["deleteBtn"], {
+      id: `${personsList[i].id}`,
+    });
+    //add event listener for delete
+    const newItem = createElement(
+      "li",
+      [personsList[i].name, deleteBtn],
+      ["person", "list-group-item"]
+    );
+    //eventListener for new item
+    personsListElement.append(newItem);
+  }
+};
+
+const cleanPersonsList = () => {
+  personsListElement.innerHTML = "";
+};
+
+const createElement = (
+  tagName,
+  children = [],
+  classes = [],
+  attributes = {}
+) => {
+  // create new element in more comfortable
+  const el = document.createElement(tagName);
+  for (let child of children) {
+    // append childs of element
+    el.append(child);
+  }
+  for (let cls of classes) {
+    // add all the classes to the element
+    el.classList.add(cls);
+  }
+  for (let attr in attributes) {
+    // add all attributes to the element
+    el.setAttribute(attr, attributes[attr]);
+  }
+  return el;
+};
 addButton.addEventListener("click", addNewPerson);
